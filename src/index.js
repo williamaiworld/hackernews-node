e944f1a3da03297da67ddfad018a38fbed147794
William Klemm <williamaiworld@gmail.com>
2019-05-16 08:34:38 -0700

    working schema and resolvers for info and feed.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
 const { GraphQLServer } = require('graphql-yoga')
-
+require('dotenv').config();
 // 1
+
 const typeDefs = `
 type Query {
   info: String!
+  feed: [Link!]!
+}
+type Link {
+  id: ID!
+  description: String!
+  url: String!
 }
 `
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+},
+{
+  id: 'link-1',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL, SECOND example'
+},
+{
+  id: 'link-2',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL, THIRD example'
+}]
 
 // 2
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+  Link: {
+    // id: (parent) => parent.id,
+    // description: (parent) => parent.description,
+    // url: (parent) => parent.url,
   }
 }
 const options = {
-  port: 8000,
+  port: process.env.PORT,
 }
 
 // 3
@@ -22,5 +50,6 @@ const server = new GraphQLServer({
   typeDefs,
   resolvers,
 })
+console.log('port: ', process.env.PORT)
 
 server.start(options, () => console.log(`Server is running on http://localhost:${options.port}`))
